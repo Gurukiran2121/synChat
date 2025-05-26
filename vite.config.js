e752492f51f2c4c1da877d726035231d6fb979a1
Gurukiran SN <gurukiran.sn@netenrich.com>
2025-05-26 18:23:40 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const isProd = mode === "production";
 
   return {
-    server: { port: 4008},
+    server: { port: 4008 },
     build: { target: "esnext" },
     plugins: [
       react(),
@@ -17,10 +17,6 @@ export default defineConfig(({ mode }) => {
           authApp: isProd
             ? "https://authmicroapp.netlify.app/assets/remoteEntry.js"
             : "http://localhost:4006/assets/remoteEntry.js",
-          // And drop the stray "main_chat_app@" prefix for Vite→Vite
-          chatHomeApp: isProd
-            ? "https://your-prod-chathome.app/remoteEntry.js"
-            : "http://localhost:4010/remoteEntry.js",
         },
         shared: ["react", "react-dom"],
       }),
